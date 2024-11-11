71e52e1d96c0b35beb4c68db966fe0c36f019e77
Piotr <piotr.lange@ploonit.group>
2024-11-11 15:43:11 +0100

    Ubung 12 changes 11.11.2024

--- Ubung12/Program.cs
+++ Ubung12/Program.cs
@@ -5,6 +5,7 @@ using System.Net.Security;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Ubung12
 {
@@ -12,47 +13,19 @@ namespace Ubung12
     {
         static void Main(string[] args)
         {
+          void SayHello(string name) 
+            {
+            Console.WriteLine("Hallo " + name);
+                Console.WriteLine("Wie gehts");
+                Console.WriteLine("----");
+            }
+            SayHello("Alina");
+            SayHello("Darek");
 
-
-            Random random = new Random();
-            int daysUntilExpiration = random.Next(12);
-            int discountPercentage = 0;
-
-            Console.WriteLine(daysUntilExpiration);
-            
-            if (daysUntilExpiration >= 10)
-            {
-                Console.WriteLine("Your subscription will expire soon. Renew now! ");
-            }
-            if (daysUntilExpiration < 10 || daysUntilExpiration >= 5 )
-            {
-                Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days. ");
-                discountPercentage = 10;
-            }
-            if (daysUntilExpiration <= 4)
-            {
-                Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days. ");
-                discountPercentage = 20;
-            }
-            if (daysUntilExpiration == 0)
-            {
-                Console.WriteLine($"Your subscription has expired");
-            }
-            if (daysUntilExpiration < 10)
-            {
-                Console.WriteLine($"Renew now and save {discountPercentage}%.");
-            }
             Console.ReadLine();
-
-               
-
-
-
-
-
-
         }
 
     }
-    
-}
+
+    }
+
